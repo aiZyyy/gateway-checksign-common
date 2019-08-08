ac577eb12322fd1a945212e6cfea8a49f047fed6
MiaoWoo <admin@yumc.pw>
2019-08-08 09:12:52 +0800

    Create & Init gateway-checksign-service Project...

---
+++ src/main/java/com/sixi/gateway/checksignservice/GatewayCheckSignServiceApplication.java
@@ -0,0 +1,17 @@
+package com.sixi.gateway.checksignservice;
+
+import org.springframework.boot.SpringApplication;
+
+import com.sixi.micro.common.annotation.SixiMicroServiceApplication;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @author MiaoWoo
+ */
+@SixiMicroServiceApplication
+public class GatewayCheckSignServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayCheckSignServiceApplication.class, args);
+    }
+}
